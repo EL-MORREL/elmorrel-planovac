084d3cf5bb65abfe964de434d390cab086e51be0
EL-MORREL <radek@el-morrel.cz>
2026-06-03 11:51:36 +0200

    Update realtime.js

--- js/realtime.js
+++ js/realtime.js
@@ -11,82 +11,51 @@ async function startRealtime() {
   }
 
   realtimeChannel = supabaseClient
-    .channel("app_state_changes")
+    .channel("planner_realtime")
+
     .on(
       "postgres_changes",
       {
         event: "*",
         schema: "public",
-        table: "app_state"
+        table: "jobs"
       },
-     (payload) => {
-
-  console.log("Realtime payload:", payload);
-
-  if (!payload.new?.data) return;
-
-  const incoming = payload.new.data;
-
-if (
-  payload.new.updated_by === currentUser.email
-){
-  return;
-}
-
-if(
-  JSON.stringify(incoming) ===
-  JSON.stringify(db)
-){
-  return;
-}
-if(draggingNow){
-
-  setTimeout(() => {
-    db = incoming;
-    render();
-  }, 1000);
-
-  return;
-}
-console.log(
-  "Realtime update",
-  incoming.jobs?.length,
-  "zakázek"
-);       
-db = incoming;
-
-  if (!db.notes) {
-    db.notes = [];
-  }
-
-  if (!db.absences) {
-    db.absences = [];
-  }
-
-  if (!db.vehicleAbsences) {
-    db.vehicleAbsences = [];
-  }
-
-  setTimeout(() => {
-  render();
-}, 50);
-
-  setStatus("Aktualizováno z cloudu");
-}
+      async () => {
+        if(draggingNow) return;
+        await loadDb();
+        setStatus("Aktualizováno z cloudu");
+      }
     )
+
+    .on(
+      "postgres_changes",
+      {
+        event: "*",
+        schema: "public",
+        table: "assignments"
+      },
+      async () => {
+        if(draggingNow) return;
+        await loadDb();
+        setStatus("Aktualizováno z cloudu");
+      }
+    )
+
     .subscribe((status) => {
+
       console.log("Realtime status:", status);
 
-      if (status === "SUBSCRIBED") {
+      if(status === "SUBSCRIBED"){
         console.log("Realtime připojen");
       }
 
-      if (status === "CHANNEL_ERROR") {
+      if(status === "CHANNEL_ERROR"){
         console.error("Realtime channel error");
       }
 
-      if (status === "TIMED_OUT") {
+      if(status === "TIMED_OUT"){
         console.error("Realtime timeout");
       }
+
     });
 }
